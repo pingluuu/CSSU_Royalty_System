cacae23cc588291e73daf249c0dea8e5b72e3467
Ethan Hsu <ethanhsu@ethans-air-2.home>
2025-04-01 18:36:21 -0400

    added protected routes, forbidden placeholder page

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,21 @@ import LoginPage from "./pages/LoginPage";
 import NavBar from "./components/NavBar";
 // import "./App.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
-
-
+import ProtectedRoute from "./components/protectedRoutes";
+import ManagerCreateNewEvent from "./pages/manager/CreateEventPage";
+import ForbiddenPage from "./pages/Forbidden";
 const AppRoutes = () => {
 	const {user} = useAuth();
 	return (
 		<Routes>
 			<Route path="/login" element={!user? <LoginPage/>: <Navigate to="/"/>}/>
+			<Route path="/manager/events/new" element=
+			{<ProtectedRoute requiredRole="manager">
+				<ManagerCreateNewEvent/>
+			</ProtectedRoute>}/>
+			
 			<Route path = "/" element={<div>THIS IS JUST PLACEHOLDER PAGE</div>}/>
+			<Route path = "/forbidden" element={<ForbiddenPage/>}/>
 			<Route path="*" element={<div>404 - Page Not Found</div>} />
 		</Routes>
 	)
---
+++ src/pages/Forbidden.tsx
@@ -0,0 +1,7 @@
+const ForbiddenPage = () => {
+    return (<div>
+        THIS PAGE IS TO SHOW NOT ENOUGH AUTHORIZATED
+    </div>)
+}
+
+export default ForbiddenPage
